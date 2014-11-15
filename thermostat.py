0917d6deac5501101d0f29e56eb917ee72bc462b
Acolab <contact@acolab.fr>
2014-11-15 16:23:54 +0000

    created temperatures table

--- thermostat.py
+++ thermostat.py
@@ -1,4 +1,24 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float, DateTime
+
+engine = create_engine('sqlite:///thermostat.sqlite', echo=True)
+Base = declarative_base()
+
+class User(Base):
+	__tablename__ = 'temperatures'
+
+	id = Column(Integer, primary_key=True)
+	date = Column(DateTime)
+	temperature = Column(Float)
+
+	def __repr__(self):
+		return "<Temperature(date='%s', temperature='%s'>" % (
+                             self.date, self.temperature)
+
+Base.metadata.create_all(engine)
+
 app = Flask(__name__)
 
 @app.route('/')
