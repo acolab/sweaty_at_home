376e362e40da42fe4e5523c84944f903e1a5b4ac
Acolab <contact@acolab.fr>
2014-11-15 16:52:41 +0000

    display temp list

--- thermostat.py
+++ thermostat.py
@@ -1,13 +1,20 @@
 from flask import Flask
 from flask import request
 from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Float, DateTime
 from sqlalchemy.orm import sessionmaker
 import datetime
+from flask import render_template
 
-engine = create_engine('sqlite:///thermostat.sqlite', echo=True)
+
+engine = create_engine('sqlite:///thermostat.sqlite', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
 Base = declarative_base()
+Base.query = db_session.query_property()
 
 class Temperature(Base):
 	__tablename__ = 'temperatures'
@@ -20,23 +27,25 @@ class Temperature(Base):
 		return "<Temperature(date='%s', temperature='%s'>" % (
                              self.date, self.temperature)
 
-Base.metadata.create_all(engine)
-
-Session = sessionmaker(bind=engine)
-session = Session()
+Base.metadata.create_all(bind=engine)
 
 app = Flask(__name__)
 
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+	temperatures = Temperature.query.all()
+	return render_template('index.html', temperatures=temperatures)
 
 @app.route('/new-temperature')
 def new_temperature():
 	temperature = Temperature(temperature=request.args.get("value"), date=datetime.datetime.now())
-	session.add(temperature)
-	session.commit()
-	return temperatre.id
+	db_session.add(temperature)
+	db_session.commit()
+	return str(temperature.id)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
